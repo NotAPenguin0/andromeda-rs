ec998166cf6325e0bf1d6f877e5e858520ee3ecc
NotAPenguin <michiel.vandeginste@gmail.com>
2023-05-15 11:22:26 +0200

    add: sense clicking the world view window

--- crates/gui/src/editor/world_view.rs
+++ crates/gui/src/editor/world_view.rs
@@ -1,10 +1,20 @@
+use egui::Response;
 use inject::DI;
+use log::trace;
 use scheduler::EventBus;
 use util::SafeUnwrap;
 
 use crate::editor::camera_controller::enable_camera_over;
 use crate::util::image_provider::ImageProvider;
 use crate::widgets::resizable_image::resizable_image_window;
+
+fn behaviour(response: Response, bus: &EventBus<DI>) {
+    enable_camera_over(&response, bus).safe_unwrap();
+
+    if response.clicked() {
+        trace!("Clicked the window");
+    }
+}
 
 /// Show the world view
 /// # DI Access
@@ -19,7 +29,7 @@ pub fn show(context: &egui::Context, bus: &EventBus<DI>) {
             provider.size = size.into();
             provider.handle
         },
-        |response| enable_camera_over(&response, bus).safe_unwrap(),
+        |response| behaviour(response, bus),
         (800.0, 600.0).into(),
     );
 }

8d74185f005905045ef4f14b8d61962e2f379e5e
NotAPenguin <michiel.vandeginste@gmail.com>
2023-05-17 11:06:10 +0200

    add: gaussian blur on height brush

--- crates/brush/src/lib.rs
+++ crates/brush/src/lib.rs
@@ -66,7 +66,7 @@ fn record_update_normals<'q>(
         vk::AccessFlags2::NONE,
         vk::AccessFlags2::MEMORY_READ | vk::AccessFlags2::MEMORY_WRITE,
     );
-    const NORMAL_SIZE: u32 = SIZE + 4;
+    const NORMAL_SIZE: u32 = SIZE + 8;
     let cmd = cmd.bind_compute_pipeline("normal_recompute")?;
     let cmd = cmd
         .bind_storage_image(0, 0, &normals.image.image.view)?
@@ -88,6 +88,25 @@ fn record_update_normals<'q>(
         vk::AccessFlags2::MEMORY_READ | vk::AccessFlags2::MEMORY_WRITE,
         vk::AccessFlags2::NONE,
     );
+    Ok(cmd)
+}
+
+fn record_height_blur<'q>(
+    cmd: IncompleteCommandBuffer<'q, All>,
+    uv: Vec2,
+    heights: &Heightmap,
+) -> Result<IncompleteCommandBuffer<'q, All>> {
+    const BLUR_SIZE: u32 = SIZE + 4;
+    let cmd = cmd.bind_compute_pipeline("blur_rect")?;
+    let cmd = cmd
+        .bind_storage_image(0, 0, &heights.image.image.view)?
+        .push_constant(vk::ShaderStageFlags::COMPUTE, 0, &uv)
+        .push_constant(vk::ShaderStageFlags::COMPUTE, 8, &BLUR_SIZE)
+        .dispatch(
+            (BLUR_SIZE as f32 / 16.0f32).ceil() as u32,
+            (BLUR_SIZE as f32 / 16.0f32).ceil() as u32,
+            1,
+        )?;
     Ok(cmd)
 }
 
@@ -116,6 +135,14 @@ fn record_update_commands(
         .push_constant(vk::ShaderStageFlags::COMPUTE, 0, &uv)
         .push_constant(vk::ShaderStageFlags::COMPUTE, 8, &SIZE)
         .dispatch(SIZE / 16, SIZE / 16, 1)?;
+    // Add a barrier to synchronize with the blur shader
+    let cmd = cmd.memory_barrier(
+        PipelineStage::COMPUTE_SHADER,
+        vk::AccessFlags2::MEMORY_READ | vk::AccessFlags2::MEMORY_WRITE,
+        PipelineStage::COMPUTE_SHADER,
+        vk::AccessFlags2::MEMORY_READ | vk::AccessFlags2::MEMORY_WRITE,
+    );
+    // let cmd = record_height_blur(cmd, uv, heights)?;
     // Transition back to ShaderReadOnlyOptimal for drawing. This also synchronizes access to the heightmap
     // with the normal map calculation shader
     let cmd = cmd.transition_image(
@@ -251,6 +278,11 @@ fn create_brush_pipeline(bus: &EventBus<DI>) -> Result<()> {
         .persistent()
         .into_dynamic()
         .set_shader("shaders/src/normal_recompute.cs.hlsl")
+        .build(bus, gfx.pipelines.clone())?;
+    ComputePipelineBuilder::new("blur_rect")
+        .persistent()
+        .into_dynamic()
+        .set_shader("shaders/src/blur_rect.cs.hlsl")
         .build(bus, gfx.pipelines)?;
     Ok(())
 }

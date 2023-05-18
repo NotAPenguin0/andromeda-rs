a9f0fdeb06f74ebbcdb46abadcd9771410b42f58
NotAPenguin <michiel.vandeginste@gmail.com>
2023-05-18 16:37:29 +0200

    fix: do not fail build script if shader out dir already exists

--- crates/app/build.rs
+++ crates/app/build.rs
@@ -1,6 +1,7 @@
 fn main() -> std::io::Result<()> {
     // We cannot commit empty directories on git, so create the output
     // directory for shaders here
-    std::fs::create_dir("../../shaders/src/out")?;
+
+    std::fs::create_dir_all("../../shaders/src/out")?;
     Ok(())
 }
